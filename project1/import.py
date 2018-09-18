8d281009b8ba2542dfca04f39bc8313cc28508e6
alexago <alexago1@yandex.ru>
2018-09-18 12:41:45 +0300

    Add import.py - adding books into DB from CSV file

---
+++ project1/import.py
@@ -0,0 +1,21 @@
+import os
+import csv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+	f = open("books.csv")
+	reader = csv.reader(f)
+	#skip header
+	next(reader, None)
+	
+	for isbn,title,author,year in reader:
+		sql_str = "INSERT INTO books (isbn,title,author,year) VALUES (:isbn, :title, :author, :year)"
+		db.execute(sql_str, {"isbn":isbn, "title":title, "author":author, "year":year})
+	db.commit()
+	
+if __name__ == "__main__":
+	main()
